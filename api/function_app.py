7f5bd336a4f7667d695f51120c92bf9bad77aab7
PrettyMing <pro.mingian@gmail.com>
2026-02-04 11:33:29 -0600

    Final boss: connect cosmosdb

--- api/function_app.py
+++ api/function_app.py
@@ -1,16 +1,28 @@
 import azure.functions as func
 import json
+import os
+from azure.cosmos import CosmosClient
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 @app.route(route="visitor_counter")
 def visitor_counter(req: func.HttpRequest) -> func.HttpResponse:
-    # For now, we will return a hardcoded number. 
-    # Once the DB is connected, we will change this logic.
-    mock_count = 124 
+    # 1. Get the connection string from environment variables
+    conn_str = os.environ["CosmosDBConnectionString"]
+    client = CosmosClient.from_connection_string(conn_str)
     
+    # 2. Connect to the database and container
+    database = client.get_database_client("AzureResume")
+    container = database.get_container_client("Counter")
+    
+    # 3. Read the item (id="1"), increment it, and update it
+    item = container.read_item(item="1", partition_key="1")
+    item["count"] += 1
+    container.replace_item(item="1", body=item)
+    
+    # 4. Return the new count
     return func.HttpResponse(
-        body=json.dumps({"count": mock_count}),
-        status_code=200,
-        mimetype="application/json"
+        json.dumps({"count": item["count"]}),
+        mimetype="application/json",
+        status_code=200
     )
